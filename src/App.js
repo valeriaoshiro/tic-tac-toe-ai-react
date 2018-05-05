df71a641c2b30059b0621752dac92991f9df616e
Valeria Oshiro <valeriaoshiro19@gmail.com>
2018-05-04 20:16:41 -0700

    Initial setup. App.js setup

--- src/App.js
+++ src/App.js
@@ -3,16 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      turn: 'X',
+      board: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+      message: ''
+    }
+    this.handleClick = this.handleClick.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
+  handleClick(e){
+    e.preventDefault();
+  }
+  handleReset(e){
+    e.preventDefault();
+    const turn = 'X';
+    const board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    const message: ''
+    this.setState({turn, board, message});
+  }
+
   render() {
+    const squares = this.state.board.map((s, i) => {
+      <Square id={i} key={i} onClick={this.handleClick}/>
+    })
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Unbetable Tic Tac Toe</h1>
+        {squares}
+        <ResetButton />
+        <Message />
+        <button onClick={this.handleReset}>Reset</button>
+        <h2></h2>
       </div>
     );
   }
